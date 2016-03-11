bc209e51048554c4769be42c36ed2370fe4b474a
Julien HENRY <julien.henry@sonarsource.com>
2016-03-11 11:13:21 +0100

    Update rules for Eclipse and IntelliJ. Add IntelliJ 1.5.0

--- intellij/resources/scripts/App.ts
+++ intellij/resources/scripts/App.ts
@@ -1,3 +1,3 @@
 ﻿window.onload = () => {
-    App.Controller = new Controllers.RulePageController('IntelliJ', '1.4.0', 20);
+    App.Controller = new Controllers.RulePageController('IntelliJ', '1.5.0', 20);
 }
