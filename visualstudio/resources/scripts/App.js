290bd0e3a5004056d5ae1daeba58997cd083e146
Tamas Vajk <tamas.vajk@sonarsource.com>
2016-05-18 09:20:19 +0200

    VS: changes for version 2.2.1

--- visualstudio/resources/scripts/App.js
+++ visualstudio/resources/scripts/App.js
@@ -1,9 +1,10 @@
 var sonarLintSonarAnalyzerMappings = [
     { sonarLintVersion: '2.0.0', sonarAnalyzerVersion: '1.10.0' },
     { sonarLintVersion: '2.1.0', sonarAnalyzerVersion: '1.11.0' },
-    { sonarLintVersion: '2.2.0', sonarAnalyzerVersion: '1.12.0' }
+    { sonarLintVersion: '2.2.0', sonarAnalyzerVersion: '1.12.0' },
+    { sonarLintVersion: '2.2.1', sonarAnalyzerVersion: '1.13.0' }
 ];
 window.onload = function () {
-    App.Controller = new Controllers.VisualStudioRulePageController('1.12.0');
+    App.Controller = new Controllers.VisualStudioRulePageController('1.13.0');
 };
 //# sourceMappingURL=App.js.map
--- visualstudio/resources/scripts/App.ts
+++ visualstudio/resources/scripts/App.ts
@@ -6,9 +6,10 @@
 var sonarLintSonarAnalyzerMappings: SonarLintSonarAnalyzerMapping[] = [
     { sonarLintVersion: '2.0.0', sonarAnalyzerVersion: '1.10.0' },
     { sonarLintVersion: '2.1.0', sonarAnalyzerVersion: '1.11.0' },
-    { sonarLintVersion: '2.2.0', sonarAnalyzerVersion: '1.12.0' }
+    { sonarLintVersion: '2.2.0', sonarAnalyzerVersion: '1.12.0' },
+    { sonarLintVersion: '2.2.1', sonarAnalyzerVersion: '1.13.0' }
 ];
 
 window.onload = () => {
-    App.Controller = new Controllers.VisualStudioRulePageController('1.12.0');
+    App.Controller = new Controllers.VisualStudioRulePageController('1.13.0');
 }
