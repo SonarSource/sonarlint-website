cb8242aa636fb603f5803eb938b2544518a763ec
Julien HENRY <julien.henry@sonarsource.com>
2016-07-29 19:37:00 +0200

    Release SonarLint Eclipse 2.2.0

--- eclipse/resources/scripts/App.js
+++ eclipse/resources/scripts/App.js
@@ -1,4 +1,4 @@
 window.onload = function () {
-    App.Controller = new Controllers.RulePageController('Eclipse', '2.1.0', 20);
+    App.Controller = new Controllers.RulePageController('Eclipse', '2.2.0', 20);
 };
 //# sourceMappingURL=App.js.map
