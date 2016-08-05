2021aedc1d7a918686d49ea3fcfe04ad00154deb
Duarte Meneses <duarte.meneses@sonarsource.com>
2016-08-05 15:21:42 +0200

    Release SonarLint IntelliJ v2.3

--- intellij/resources/scripts/App.js
+++ intellij/resources/scripts/App.js
@@ -1,4 +1,4 @@
 window.onload = function () {
-    App.Controller = new Controllers.RulePageController('IntelliJ', '2.2', 20);
+    App.Controller = new Controllers.RulePageController('IntelliJ', '2.3', 20);
 };
 //# sourceMappingURL=App.js.map
--- intellij/resources/scripts/App.ts
+++ intellij/resources/scripts/App.ts
@@ -1,3 +1,3 @@
 ﻿window.onload = () => {
-    App.Controller = new Controllers.RulePageController('IntelliJ', '2.2', 20);
+    App.Controller = new Controllers.RulePageController('IntelliJ', '2.3', 20);
 }
