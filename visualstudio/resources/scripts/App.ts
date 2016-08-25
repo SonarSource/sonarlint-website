c15dee6d884c6d0dd448e9312b6cca055589ca2c
Tamas Vajk <tamas.vajk@sonarsource.com>
2016-08-25 14:07:24 +0200

    Bugfix: Add missing comma in TS file

--- visualstudio/resources/scripts/App.ts
+++ visualstudio/resources/scripts/App.ts
@@ -10,7 +10,7 @@ var sonarLintSonarAnalyzerMappings: SonarLintSonarAnalyzerMapping[] = [
     { sonarLintVersion: '2.2.1', sonarAnalyzerVersion: '1.13.0' },
     { sonarLintVersion: '2.3.0', sonarAnalyzerVersion: '1.14.0' },
     { sonarLintVersion: '2.4.0', sonarAnalyzerVersion: '1.15.0' },
-    { sonarLintVersion: '2.5.0', sonarAnalyzerVersion: '1.16.0' }
+    { sonarLintVersion: '2.5.0', sonarAnalyzerVersion: '1.16.0' },
     { sonarLintVersion: '2.6.0', sonarAnalyzerVersion: '1.17.0' }
 ];
 
