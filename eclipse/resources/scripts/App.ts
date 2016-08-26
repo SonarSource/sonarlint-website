f0225847177546e4dc97cd4096d01e4cc8ec6418
Julien HENRY <julien.henry@sonarsource.com>
2016-08-26 16:33:04 +0200

    SLE 2.2.1

--- eclipse/resources/scripts/App.ts
+++ eclipse/resources/scripts/App.ts
@@ -1,3 +1,3 @@
 ﻿window.onload = () => {
-    App.Controller = new Controllers.RulePageController('Eclipse', '2.2.0', 20);
+    App.Controller = new Controllers.RulePageController('Eclipse', '2.2.1', 20);
 }
