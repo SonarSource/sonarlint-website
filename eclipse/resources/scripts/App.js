b951d6b3c4df77d2acb99e71b847ae5d3970ed16
Janos Gyerik <janos.gyerik@sonarsource.com>
2016-11-16 13:50:22 +0100

    Release SLE 2.3.0

--- eclipse/resources/scripts/App.js
+++ eclipse/resources/scripts/App.js
@@ -1,3 +1,3 @@
 window.onload = function () {
-    App.Controller = new Controllers.RulePageController('Eclipse', '2.2.1', 20);
+    App.Controller = new Controllers.RulePageController('Eclipse', '2.3.0', 20);
 };
