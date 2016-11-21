52dead77300a0d1a1851c7882651350e139ab680
Duarte Meneses <duarte.meneses@sonarsource.com>
2016-11-21 10:25:23 +0100

    Release SLI 2.4.2

--- intellij/resources/scripts/App.ts
+++ intellij/resources/scripts/App.ts
@@ -1,3 +1,3 @@
 ﻿window.onload = () => {
-    App.Controller = new Controllers.RulePageController('IntelliJ', '2.4.1', 20);
+    App.Controller = new Controllers.RulePageController('IntelliJ', '2.4.2', 20);
 }
