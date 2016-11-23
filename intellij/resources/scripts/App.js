1bed2992c107df4e4293cdba1d7fef9f8bb707a0
Janos Gyerik <janos.gyerik+windows@sonarsource.com>
2016-11-23 12:46:56 +0100

    Release SLE 2.4.3

--- intellij/resources/scripts/App.js
+++ intellij/resources/scripts/App.js
@@ -1,3 +1,3 @@
 window.onload = function () {
-    App.Controller = new Controllers.RulePageController('IntelliJ', '2.4.2', 20);
+    App.Controller = new Controllers.RulePageController('IntelliJ', '2.4.3', 20);
 };
