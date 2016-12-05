4cb446e1e2516c4d2aa0f8990571fb5e829f2d29
Julien HENRY <julien.henry@sonarsource.com>
2016-12-05 15:58:10 +0100

    Release SLE 2.4.0

--- eclipse/resources/scripts/App.js
+++ eclipse/resources/scripts/App.js
@@ -1,3 +1,3 @@
 window.onload = function () {
-    App.Controller = new Controllers.RulePageController('Eclipse', '2.3.1', 20);
+    App.Controller = new Controllers.RulePageController('Eclipse', '2.4.0', 20);
 };
