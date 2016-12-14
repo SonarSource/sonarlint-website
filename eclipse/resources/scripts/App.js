df80b74de50665f3cdb653b3e4d2d7f6c195200b
Julien HENRY <henryju@yahoo.fr>
2016-12-14 14:36:41 +0100

    Release SLE 2.5.0

--- eclipse/resources/scripts/App.js
+++ eclipse/resources/scripts/App.js
@@ -1,4 +1,4 @@
 window.onload = function () {
-    App.Controller = new Controllers.RulePageController('Eclipse', '2.4.0', 20);
+    App.Controller = new Controllers.RulePageController('Eclipse', '2.5.0', 20);
 };
 //# sourceMappingURL=App.js.map
