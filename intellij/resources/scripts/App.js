e99d8059afe2752f582356b682f12a5da7d90f3f
Duarte Meneses <duarte.meneses@sonarsource.com>
2017-05-23 17:18:56 +0200

    Release SLI 2.10

--- intellij/resources/scripts/App.js
+++ intellij/resources/scripts/App.js
@@ -1,4 +1,4 @@
 window.onload = function () {
-    App.Controller = new Controllers.RulePageController('IntelliJ', '2.9', 20);
+    App.Controller = new Controllers.RulePageController('IntelliJ', '2.10', 20);
 };
 //# sourceMappingURL=App.js.map
--- intellij/resources/scripts/App.ts
+++ intellij/resources/scripts/App.ts
@@ -1,3 +1,3 @@
 ﻿window.onload = () => {
-    App.Controller = new Controllers.RulePageController('IntelliJ', '2.9', 20);
+    App.Controller = new Controllers.RulePageController('IntelliJ', '2.10', 20);
 }
