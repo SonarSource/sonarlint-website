90761b9ec2ff9234e87f8814e106e529bd1118f3
Julien HENRY <julien.henry@sonarsource.com>
2017-07-03 10:52:28 +0200

    Release VSCode 1.1.0

--- vscode/resources/scripts/App.ts
+++ vscode/resources/scripts/App.ts
@@ -1,3 +1,3 @@
 ﻿window.onload = () => {
-    App.Controller = new Controllers.RulePageController('VS Code', '1.0.0', 20);
+    App.Controller = new Controllers.RulePageController('VS Code', '1.1.0', 20);
 }
