9b4d8fce8407a3463e561fb7494b8a035d31639d
Valeri Hristov <valeri.hristov@sonarsource.com>
2017-08-02 15:17:50 +0200

    Add SonarLint for VS 3.4.0-RC1

--- visualstudio/resources/scripts/App.ts
+++ visualstudio/resources/scripts/App.ts
@@ -1,4 +1,4 @@
-﻿interface SonarLintSonarAnalyzerMapping {
+interface SonarLintSonarAnalyzerMapping {
     sonarLintVersion: string;
     sonarAnalyzerVersion: string;
 }
@@ -35,7 +35,8 @@ var sonarLintSonarAnalyzerMappings: SonarLintSonarAnalyzerMapping[] = [
     { sonarLintVersion: '3.1.0', sonarAnalyzerVersion: '6.0.0.2033' },
     { sonarLintVersion: '3.2.0-RC1', sonarAnalyzerVersion: '6.1.0.2272' },
     { sonarLintVersion: '3.2.1', sonarAnalyzerVersion: '6.1.0.2359' },
-    { sonarLintVersion: '3.3.0', sonarAnalyzerVersion: '6.2.0.2536' }
+    { sonarLintVersion: '3.3.0', sonarAnalyzerVersion: '6.2.0.2536' },
+    { sonarLintVersion: '3.4.0-RC1', sonarAnalyzerVersion: '6.3.0.2784' }
 ];
 
 window.onload = () => {
