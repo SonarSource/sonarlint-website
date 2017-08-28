4b1fea8fe7007936a11f18904adce056d4dba91f
Valeri Hristov <valeri.hristov@sonarsource.com>
2017-08-28 11:48:28 +0200

    Add SonarLint for VS 3.5.0-RC1

--- visualstudio/resources/scripts/App.js
+++ visualstudio/resources/scripts/App.js
@@ -32,7 +32,8 @@ var sonarLintSonarAnalyzerMappings = [
     { sonarLintVersion: '3.2.1', sonarAnalyzerVersion: '6.1.0.2359' },
     { sonarLintVersion: '3.3.0', sonarAnalyzerVersion: '6.2.0.2536' },
     { sonarLintVersion: '3.4.0-RC1', sonarAnalyzerVersion: '6.3.0.2784' },
-    { sonarLintVersion: '3.4.0', sonarAnalyzerVersion: '6.3.0.2862' }
+    { sonarLintVersion: '3.4.0', sonarAnalyzerVersion: '6.3.0.2862' },
+    { sonarLintVersion: '3.5.0-RC1', sonarAnalyzerVersion: '6.4.0.3302' }
 ];
 window.onload = function () {
     App.Controller = new Controllers.VisualStudioRulePageController('6.3.0.2862');
